3404b6435778038f4d837a538c2c832c6f83cf8f
johnoppenheimer <maxime.cattet@epita.fr>
2013-11-23 16:21:53 +0100

    Ajout des boutons sur la page d’accueil

---
+++ Mobidevland/Mobidevland/EPInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  EPInfoViewController.h
+//  Mobidevland
+//
+//  Created by Maxime Cattet on 23/11/2013.
+//  Copyright (c) 2013 EpiPortal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EPInfoViewController : UIViewController
+
+@end
